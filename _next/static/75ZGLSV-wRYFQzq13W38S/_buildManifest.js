73ad6a3f927cdc88cf96d668523ae751458d14c4
kamin72 <a1616001@gmail.com>
2024-08-02 19:26:46 +0800

    Updates

--- _next/static/75ZGLSV-wRYFQzq13W38S/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/615-d97946c39496bc8a.js","static/chunks/pages/index-347cfbf39421d5c1.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
