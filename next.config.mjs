02e88306a2a77b5a779104cc761ebf04998650ef
kamin72 <a1616001@gmail.com>
2024-08-02 19:57:32 +0800

    Configure for gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,11 @@ const isProd = process.env.NODE_ENV === "production";
 const repoName = "wedding"; // 替换为你的仓库名
 
 const nextConfig = {
-  images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
   basePath: isProd ? `/${repoName}` : "",
   assetPrefix: isProd ? `/${repoName}/` : "",
   output: "export",
   images: {
+    domains: ["lh3.googleusercontent.com"],
     unoptimized: true,
   },
 };
