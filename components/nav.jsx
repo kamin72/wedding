04760354c4ff80aeb9c27274598aadef779894fa
kamin72 <a1616001@gmail.com>
2024-08-02 20:00:16 +0800

    Configure for gh-pages

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import flowerImage from "../public/flower.PNG";
 
 export default function Nav() {
-  const basePath = process.env.NODE_ENV === "production" ? `/${repoName}` : "";
+  //   const basePath = process.env.NODE_ENV === "production" ? `/${repoName}` : "";
   return (
     <>
       <div className="text-center">
         <Image
-          src={`${basePath}/flower.PNG`}
+          src={flowerImage}
           alt="flower"
           width={1000}
           height={200}
