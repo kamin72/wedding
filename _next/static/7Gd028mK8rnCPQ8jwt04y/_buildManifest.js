0180d57b2690e3e22d87385e11bae256579c1559
kamin72 <a1616001@gmail.com>
2024-08-13 14:24:12 +0800

    Updates

--- _next/static/7Gd028mK8rnCPQ8jwt04y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/615-721bb1c09fa639d5.js","static/chunks/pages/index-fa5b9a97df771667.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
